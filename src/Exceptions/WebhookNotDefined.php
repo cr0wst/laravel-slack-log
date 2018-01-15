e35c9c51d4ec1e773dc8fa014238581cc9d758d3
Steve Crow <steve@smcrow.net>
2018-01-15 12:44:07 -0500

    Ran PHPCS Fixer and updated LogLevel misleading comment.

--- src/Exceptions/WebhookNotDefined.php
+++ src/Exceptions/WebhookNotDefined.php
@@ -1,20 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: smcro
- * Date: 11/5/2017
- * Time: 1:29 PM
- */
 
 namespace Smcrow\SlackLog\Exceptions;
-
 
 use Throwable;
 
 class WebhookNotDefined extends \Exception
 {
-    public function __construct($message = 'Webhook configuration value must be defined.', $code = 0, Throwable $previous = null)
-    {
+    public function __construct(
+        $message = 'Webhook configuration value must be defined.',
+        $code = 0,
+        Throwable $previous = null
+    ) {
         parent::__construct($message);
     }
 }
